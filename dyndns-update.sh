e5c32c653a981d45dad786b1af119de7e7003c85
Jan Grosser <coding@jan-grosser.de>
2022-11-28 12:32:12 +0000

    Config file and debugging output

--- dyndns-update.sh
+++ dyndns-update.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-# Enter INWX DynDNS User login credentials - not Account login credentials
+## Read the configuration
 
-username='user'
-password='password'
-
-hostname='your-dyndns.domain.tld'
-
-# Optional Script setting
-
-enable_ipv6=true
-
-v4_get_url="https://v4.ident.me/"
-v6_get_url="https://v6.ident.me/"
-
-dyndns_update_url="https://dyndns.inwx.com/nic/update?"
-
-test_dns_server="1.1.1.1"
-
+source ./dyndns-update.conf
 
 ## Start Bash Script
 
@@ -29,13 +14,28 @@ ipv6=$(curl -s "$v6_get_url")
 host_ipv4=$(dig @$test_dns_server +short -t a $hostname | head -n 1)
 host_ipv6=$(dig @$test_dns_server +short -t aaaa $hostname | head -n 1)
 
+date
+echo " Current public IPv4: $ipv4"
+echo " Current DNS IPv4:    $host_ipv4"
+if [ $enable_ipv6 = true -a -n "$ipv6" ]; then
+  echo " Current public IPv6: $ipv6"
+  echo " Current DNS IPv6:    $host_ipv6"
+fi
+
 # Update DynDNS IP if not correct
 if [ $enable_ipv6 = true -a -n "$ipv6" ]; then
   if [ "$host_ipv4" != "$ipv4" -o "$host_ipv6" != "$ipv6" ]; then
+    echo -n " Updating IPv4 and IPv6 ... ("
     curl --user $username:$password "${dyndns_update_url}myip=${ipv4}&myipv6=${ipv6}"
+    echo ")"
   fi
 else
   if [ "$host_ipv4" != "$ipv4" ]; then
+    echo -n " Updating IPv4 only ... ("
     curl --user $username:$password "${dyndns_update_url}myip=${ipv4}"
+    echo ")"
   fi
 fi
+
+echo ""
+
